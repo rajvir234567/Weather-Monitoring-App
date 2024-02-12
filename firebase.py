30354c8008fac9beb4f8404c0bc026974d14276c
rajvir234567 <73382875+rajvir234567@users.noreply.github.com>
2024-02-12 14:25:25 +0000

    Add files via upload

---
+++ firebase.py
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCPRWd1wDJt-4ohHa0Vxsra1wMk8JXvFe4",
+  authDomain: "weathermonitoringapp-b2010.firebaseapp.com",
+  projectId: "weathermonitoringapp-b2010",
+  storageBucket: "weathermonitoringapp-b2010.appspot.com",
+  messagingSenderId: "994125896769",
+  appId: "1:994125896769:web:1ad952ece8b1e317be7ac0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
